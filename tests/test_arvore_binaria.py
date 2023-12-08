8d2b672792bf103b31a63738fdf3065bee350465
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-12-08 14:52:44 -0300

    :white_check_mark: Test: Teste para verificar se BinaryTree.to_binary_tree retorna corretamente uma árvore incompleta.

--- tests/test_arvore_binaria.py
+++ tests/test_arvore_binaria.py
@@ -30,3 +30,33 @@ def test_whether_the_tree_is_complete_from_the_to_binary_tree_method():
         isinstance(tree.right_tree.right_tree, BinaryTree)
         and tree.right_tree.right_tree.value == 7
     )
+
+
+def test_whether_the_tree_is_incomplete_using_the_to_binary_tree_method():
+    """Testa o método BinaryTree.to_binary_tree.
+
+    Verifca se o método está criando corretamente uma árvore binária incompleta.
+    """
+    list_ = [10, 20, 3.6, 7.1, 8, 9]
+    tree = BinaryTree.to_binary_tree(list_)
+
+    assert isinstance(tree, BinaryTree) and tree.value == 10
+    assert (
+        isinstance(tree.left_tree, BinaryTree) and tree.left_tree.value == 20
+    )
+    assert (
+        isinstance(tree.right_tree, BinaryTree)
+        and tree.right_tree.value == 3.6
+    )
+    assert (
+        isinstance(tree.left_tree.left_tree, BinaryTree)
+        and tree.left_tree.left_tree.value == 7.1
+    )
+    assert (
+        isinstance(tree.left_tree.right_tree, BinaryTree)
+        and tree.left_tree.right_tree.value == 8
+    )
+    assert (
+        isinstance(tree.right_tree.left_tree, BinaryTree)
+        and tree.right_tree.left_tree.value == 9
+    )
