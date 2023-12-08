f31aa43755ff70a221d275fecf22e302a88ed3f3
Nicolas Albuquerque Ramos <nicolasalbuquerque581@gmail.com>
2023-12-08 14:44:12 -0300

    :recycle: Remoção de fixture inutilizado.

--- tests/test_arvore_binaria.py
+++ tests/test_arvore_binaria.py
@@ -1,16 +1,11 @@
-import pytest
-
 from binary_tree import BinaryTree
 
 
-@pytest.fixture
-def binary_tree_any() -> BinaryTree:
-    """Retorna uma instância da classe BinaryTree."""
-    return BinaryTree(4, left_tree=BinaryTree(2), right_tree=BinaryTree(6))
-
-
 def test_whether_the_tree_is_complete_from_the_to_binary_tree_method():
-    """Testa a criação de uma instância da classe BinaryTree."""
+    """Testa o método BinaryTree.to_binary_tree.
+
+    Verifca se o método está criando corretamente uma árvore binária completa.
+    """
     list_ = [4, 2, 6, 1, 3, 5, 7]
     tree = BinaryTree.to_binary_tree(list_)
 
